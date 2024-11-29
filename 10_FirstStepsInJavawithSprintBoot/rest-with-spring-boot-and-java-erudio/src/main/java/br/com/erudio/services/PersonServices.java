770d48a5f40a684da28ca893c043b24544c3e47a
Celso de Oliveira Silva <celsoliveira@gmail.com>
2024-11-29 10:54:05 -0300

    Versionamento da API

---
+++ 10_FirstStepsInJavawithSprintBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,82 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.ErudioMapper;
+import br.com.erudio.mapper.custom.PersonMapper;
+import br.com.erudio.model.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.services.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonMapper mapper;
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO findById(Long id) {
+		logger.info("Finding one Person!");
+		
+		var entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+		
+		return ErudioMapper.parseObject(entity, PersonVO.class);
+	}
+	
+	public List<PersonVO> findAll() {
+		logger.info("Finding All people!");
+		return ErudioMapper.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+
+	public PersonVO create(PersonVO person) {
+		logger.info("Creating one Person!");
+		var entity = ErudioMapper.parseObject(person, Person.class);
+		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		logger.info("Updating one Person!");
+		
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deleting one Person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+
+		repository.delete(entity);
+	}
+
+	public PersonVOV2 createV2(PersonVOV2 personVo) {
+		logger.info("Creating one Person!");
+		var entity = mapper.converVoToEntity(personVo);
+		var vo = mapper.converEntityToVo(repository.save(entity));
+		return vo;
+	}
+
+}
